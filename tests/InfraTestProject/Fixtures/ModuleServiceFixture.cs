48ca1c873bf98d680d3b038e35b999675d1dbc46
MIRMOSTAFA-PC\Mirmostafa <mirmostafa@hotmail.com>
2023-03-12 10:40:32 +0330

    #11 Code code refactoring

---
+++ tests/InfraTestProject/Fixtures/ModuleServiceFixture.cs
@@ -0,0 +1,9 @@
+﻿using HanyCo.Infra.UI.Services;
+
+namespace InfraTestProject;
+
+public sealed class ModuleServiceFixture : ServicesFixture<IDtoService>
+{
+    //[Fact]
+    //public async Task
+}
