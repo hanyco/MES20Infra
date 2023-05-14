79bb2b6db7a80823528178bb2dd51864eb4053cf
MIRMOSTAFA-PC\Mirmostafa <mirmostafa@hotmail.com>
2023-05-14 19:35:30 +0330

    Checkpoint

--- src/CodeGenerator/UI/Dialogs/SelectCqrsDialog.xaml.cs
+++ src/CodeGenerator/UI/Dialogs/SelectCqrsDialog.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System.Windows;
 
+using Contracts.Services;
 using Contracts.ViewModels;
 
-using HanyCo.Infra.UI.Services;
 using HanyCo.Infra.UI.ViewModels;
 
 using Library.Exceptions.Validations;
