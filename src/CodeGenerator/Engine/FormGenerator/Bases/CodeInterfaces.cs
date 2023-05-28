0cf80baa3bce9a4b5ab44d1b2ab7e4c915d1fe07
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-05-29 00:10:46 +0330

    Checkpoint

--- src/CodeGenerator/Engine/FormGenerator/Bases/CodeInterfaces.cs
+++ src/CodeGenerator/Engine/FormGenerator/Bases/CodeInterfaces.cs
@@ -10,6 +10,11 @@ public interface IBehindCodeGenerator
 
 public interface ICodeGenerator
 {
+    /// <summary>
+    /// Generates codes based on the given parameters.
+    /// </summary>
+    /// <param name="arguments">The parameters used to generate the codes.</param>
+    /// <returns>The generated codes.</returns>
     Codes GenerateCodes(in GenerateCodesParameters? arguments = null);
 }
 
@@ -46,15 +51,29 @@ public interface ISupportsBehindCodeMember
 
 public interface IUiCodeGenerator
 {
+    /// <summary>
+    /// Generates UI code based on the given parameters.
+    /// </summary>
+    /// <param name="arguments">Optional parameters for code generation.</param>
+    /// <returns>Generated UI code.</returns>
     Code GenerateUiCode(in GenerateCodesParameters? arguments = null);
 }
 
 public interface ICodeGeneratorService<TViewModel>
 {
+    /// <summary>
+    /// Generates codes based on the given view model and optional parameters.
+    /// </summary>
+    /// <param name="viewModel">The view model to generate codes from.</param>
+    /// <param name="arguments">Optional parameters for generating codes.</param>
+    /// <returns>A result containing the generated codes.</returns>
     Result<Codes> GenerateCodes(in TViewModel viewModel, GenerateCodesParameters? arguments = null);
 }
 
 public interface IAsyncCodeGeneratorService<TViewModel>
 {
+    /// <summary>
+    /// Generates codes asynchronously based on the given view model and optional parameters.
+    /// </summary>
     Task<Result<Codes>> GenerateCodesAsync(TViewModel viewModel, GenerateCodesParameters? arguments = null, CancellationToken token = default);
 }
