768821330a68954527c03897d1edae85a3dbb669
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-08-06 11:00:47 +0330

    Separaed `ValidationResultSet` class from its extension class

--- src/Bcl/src/CoreLib/Validations/Validation.cs
+++ src/Bcl/src/CoreLib/Validations/Validation.cs
@@ -249,32 +249,3 @@ vrs.Fluent(InnerBuild(CheckBehavior.ThrowOnFail, vrs.Value, vrs.Rules));
     private static TType Invoke<TValue, TType>(Expression<Func<TValue, TType>> propertyExpression, TValue value) =>
         propertyExpression.Compile().Invoke(value);
 }
-
-[DebuggerStepThrough]
-[StackTraceHidden]
-public sealed class ValidationResultSet<TValue>(TValue value, CheckBehavior behavior, string valueName)
-{
-    internal readonly CheckBehavior Behavior = behavior;
-    internal readonly List<(Func<TValue, bool> IsValid, Func<Exception> OnError)> RuleList = new();
-    internal readonly string _valueName = valueName;
-
-    public IEnumerable<(Func<TValue, bool> IsValid, Func<Exception> OnError)> Rules => this.RuleList.ToEnumerable();
-
-    public TValue Value { get; } = value;
-
-    public static implicit operator Result<TValue>(ValidationResultSet<TValue> source) =>
-        source.Build();
-
-    public static implicit operator TValue(ValidationResultSet<TValue> source) =>
-        source.Value;
-}
-
-/// <summary>
-/// Enum to define the behavior when checking a condition.
-/// </summary>
-public enum CheckBehavior
-{
-    GatherAll,
-    ReturnFirstFailure,
-    ThrowOnFail,
-}
