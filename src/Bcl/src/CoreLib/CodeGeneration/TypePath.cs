b5ee098097f9cdd4484c9d3f31151af4f022a5a6
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-11-17 14:41:39 +0330

    Checkpoint

--- src/Bcl/src/CoreLib/CodeGeneration/TypePath.cs
+++ src/Bcl/src/CoreLib/CodeGeneration/TypePath.cs
@@ -4,14 +4,14 @@ using Library.CodeGeneration.Models;
 using Library.DesignPatterns.Markers;
 using Library.Validations;
 
-using TypeData = (string Name, string? NameSpace, System.Collections.Generic.IEnumerable<Library.CodeGeneration.TypePath> Generics);
+using TypeData = (string Name, string NameSpace, System.Collections.Generic.IEnumerable<Library.CodeGeneration.TypePath> Generics, bool IsNullable);
 
 namespace Library.CodeGeneration;
 
 [Immutable]
-public sealed class TypePath([DisallowNull] in string fullPath, in IEnumerable<string>? generics = null) : IEquatable<TypePath>
+public sealed class TypePath([DisallowNull] in string fullPath, in IEnumerable<string>? generics = null, bool? isNullable = null) : IEquatable<TypePath>
 {
-    private readonly TypeData _data = Parse(fullPath, generics);
+    private readonly TypeData _data = Parse(fullPath, generics, isNullable);
     private string? _fullName;
     private string? _fullPath;
 
@@ -26,10 +26,12 @@ public sealed class TypePath([DisallowNull] in string fullPath, in IEnumerable<s
 
     public bool IsGeneric => this._data.Generics.Any();
 
+    public bool IsNullable => this._data.IsNullable;
+
     [NotNull]
     public string Name => this._data.Name;
 
-    public string? NameSpace => this._data.NameSpace;
+    public string NameSpace => this._data.NameSpace;
 
     public static string Combine(in string? part1, params string?[] parts) =>
         StringHelper.Merge(EnumerableHelper.Iterate(part1).AddRangeImmuted(parts).Compact().Select(x => x.Trim('.')), '.');
@@ -41,10 +43,10 @@ public sealed class TypePath([DisallowNull] in string fullPath, in IEnumerable<s
     [return: NotNullIfNotNull(nameof(typePath))]
     public static string? GetNameSpace(in string? typePath) =>
         typePath == null ? null : Parse(typePath).NameSpace;
-    
-    [return: NotNullIfNotNull(nameof(typePath))]
+
+    [return: NotNull]
     public static IEnumerable<string>? GetNameSpaces(in string? typePath) =>
-        typePath == null ? null : New(typePath).GetNameSpaces();
+        typePath == null ? Enumerable.Empty<string>() : New(typePath).GetNameSpaces();
 
     [return: NotNullIfNotNull(nameof(typeInfo))]
     public static implicit operator string?(in TypePath? typeInfo) =>
@@ -59,8 +61,8 @@ public sealed class TypePath([DisallowNull] in string fullPath, in IEnumerable<s
         typeInfo == null ? null : New(typeInfo);
 
     [return: NotNull]
-    public static TypePath New([DisallowNull] in string fullPath, in IEnumerable<string>? generics = null) =>
-        new(fullPath, generics);
+    public static TypePath New([DisallowNull] in string fullPath, in IEnumerable<string>? generics = null, bool? isNullable = null) =>
+        new(fullPath, generics, isNullable);
 
     [return: NotNull]
     public static TypePath New([DisallowNull] in TypePath typePath) =>
@@ -84,8 +86,31 @@ public sealed class TypePath([DisallowNull] in string fullPath, in IEnumerable<s
     public static bool operator ==(in TypePath? left, in TypePath? right) =>
         left?.Equals(right) ?? (right is null);
 
+    public static (string Name, string NameSpace) ToKeyword(string name, string nameSpace) =>
+        nameSpace == "System"
+            ? (name switch
+            {
+                nameof(String) => ("string", ""),
+                nameof(Byte) => ("byte", ""),
+                nameof(SByte) => ("sbyte", ""),
+                nameof(Char) => ("char", ""),
+                nameof(Boolean) => ("bool", ""),
+                nameof(UInt32) => ("uint", ""),
+                nameof(IntPtr) => ("nint", ""),
+                nameof(UIntPtr) => ("nuint", ""),
+                nameof(Int16) => ("short", ""),
+                nameof(UInt16) => ("ushort", ""),
+                nameof(Int32) => ("int", ""),
+                nameof(Int64) => ("long", ""),
+                nameof(Single) => ("float", ""),
+                nameof(Decimal) => ("decimal", ""),
+                nameof(Double) => ("double", ""),
+                _ => (name, nameSpace),
+            })
+            : (name, nameSpace);
+
     public void Deconstruct(out string? name, out string? nameSpace) =>
-        (name, nameSpace) = (this.Name, this.NameSpace);
+            (name, nameSpace) = (this.Name, this.NameSpace);
 
     public void Deconstruct(out string? name, out string? nameSpace, out IEnumerable<TypePath> generics) =>
         (name, nameSpace, generics) = (this.Name, this.NameSpace, this.Generics);
@@ -121,6 +146,9 @@ public sealed class TypePath([DisallowNull] in string fullPath, in IEnumerable<s
         }
     }
 
+    public (string Name, string NameSpace) ToKeyword() =>
+        ToKeyword(this.Name, this.NameSpace);
+
     [return: NotNull]
     public override string ToString() =>
         this.GetFullPath();
@@ -129,53 +157,58 @@ public sealed class TypePath([DisallowNull] in string fullPath, in IEnumerable<s
     public TypePath ToTypePath() =>
         new(this.FullName);
 
-    private static TypeData Parse(in string typePath, in IEnumerable<string>? generics = null)
+    private static TypeData Parse(in string typePath, in IEnumerable<string>? generics = null, bool? isNullable = null)
     {
+        // Validation checks
         Check.MustBeArgumentNotNull(typePath);
         Check.MustBe(generics?.All(x => !x.IsNullOrEmpty()) ?? true, () => "Generic types cannot be null or empty.");
 
-        var temp = typePath;
+        // Initializations
+        var typePathBuffer = typePath;
         var gens = new List<string>();
-        // Find Generics
-        if (temp.Contains('<', StringComparison.Ordinal))
+
+        // Take care of nullability.
+        if (isNullable is { } nullable)
         {
-            var indexOfGenSymbol = temp.IndexOf('<', StringComparison.Ordinal);
-            var gen = temp[indexOfGenSymbol..].Trim('<').Trim('>');
-            temp = temp[..indexOfGenSymbol];
-            gens.AddRange(gen.Split(',').Select(x => x.Trim()));
-        }
-        if (temp.Contains('`', StringComparison.Ordinal))
-        {
-            temp = temp.Remove(temp.IndexOf('`', StringComparison.Ordinal), 2);
+            typePathBuffer = typePathBuffer.TrimEnd('?');
+            if (nullable)
+            {
+                typePathBuffer = typePathBuffer.AddEnd('?');
+            }
         }
 
+        // Nullability checks
+        var nullability = typePathBuffer.EndsWith('?');
+
+        // Find Generics
+        if (typePathBuffer.Contains('<', StringComparison.Ordinal))
+        {
+            var indexOfGenSymbol = typePathBuffer.IndexOf('<', StringComparison.Ordinal);
+            var gen = typePathBuffer[indexOfGenSymbol..].Trim('<').Trim('>');
+            typePathBuffer = typePathBuffer[..indexOfGenSymbol];
+            gens.AddRange(gen.Split(',').Select(x => x.Trim()));
+        }
+        if (typePathBuffer.Contains('`', StringComparison.Ordinal))
+        {
+            typePathBuffer = typePathBuffer.Remove(typePathBuffer.IndexOf('`', StringComparison.Ordinal), 2);
+        }
         if (generics?.Any() ?? false)
         {
             gens.AddRange(generics);
         }
 
         // Retrieve name and namespace
-        var lastIndexOfDot = temp.LastIndexOf('.');
+        var lastIndexOfDot = typePathBuffer.LastIndexOf('.');
         (var name, var nameSpace) = lastIndexOfDot > 0
-            ? (temp[(lastIndexOfDot + 1)..], temp[..lastIndexOfDot])
-            : (temp, string.Empty);
+            ? (typePathBuffer[(lastIndexOfDot + 1)..], typePathBuffer[..lastIndexOfDot])
+            : (typePathBuffer, string.Empty);
 
         var genTypes = gens.Select(x => new TypePath(x));
-        // CodeCOM makes a mistake.
-        //if (nameSpace == "System")
-        //{
-        //    (name, nameSpace) = name switch
-        //    {
-        //        "String" => ("string", ""),
-        //        "Boolean" => ("bool", ""),
-        //        "Int32" => ("int", ""),
-        //        "Int64" => ("long", ""),
-        //        "Single" => ("float", ""),
-        //        _ => (name, nameSpace),
-        //    };
-        //}
 
-        return (name, nameSpace, genTypes);
+        // To be more friendly, let's be kind and use C# keywords.
+        //! CodeCOM makes a mistake.
+        //x (name, nameSpace) = ToKeyword(name, nameSpace);
+        return (name, nameSpace, genTypes, nullability);
     }
 
     [return: NotNull]
