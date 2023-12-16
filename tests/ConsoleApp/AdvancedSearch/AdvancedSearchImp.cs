026b2d342a4e8b35fcdbf7f65912f351c16351db
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-12-17 02:49:35 +0330

    `WHERE` clause is constructed in alpha test. Now must be checked deeply.

--- tests/ConsoleApp/AdvancedSearch/AdvancedSearchImp.cs
+++ tests/ConsoleApp/AdvancedSearch/AdvancedSearchImp.cs
@@ -1,6 +1,9 @@
 ﻿using System.Text;
 
 using Library.CodeGeneration;
+using Library.Data.SqlServer;
+using Library.Helpers;
+using Library.Interfaces;
 
 namespace ConsoleApp.AdvancedSearch;
 
@@ -15,8 +18,7 @@ public static class AdvancedSearchImp
             {
                 _ = result.Append(" AND ");
             }
-            _ = result.Append(operation.Field.Name);
-            _ = result.Append(' ');
+            _ = result.Append($"{SqlStatementBuilder.AddBrackets(operation.Field.Name)}");
             var operand = operation.Operator switch
             {
                 AdvancedSearchFieldOperator.IsBiggerThan => $" > {operation.Parameters.First()}",
@@ -24,19 +26,38 @@ public static class AdvancedSearchImp
                 AdvancedSearchFieldOperator.Contains => $" IS LIKE('%{operation.Parameters.First()}%')",
                 AdvancedSearchFieldOperator.StartsWith => $" IS LIKE('{operation.Parameters.First()}%')",
                 AdvancedSearchFieldOperator.EndsWith => $" IS LIKE('%{operation.Parameters.First()}')",
-                AdvancedSearchFieldOperator.Equals => $" == '{operation.Parameters.First()}'",
-                AdvancedSearchFieldOperator.NotEquals => $" <> '{operation.Parameters.First()}'",
+                AdvancedSearchFieldOperator.Equals => $" == {formatOperand(operation, operation.Parameters.First())}",
+                AdvancedSearchFieldOperator.NotEquals => $" <> {formatOperand(operation, operation.Parameters.First())}",
+                AdvancedSearchFieldOperator.IsNull => " IS NULL",
+                AdvancedSearchFieldOperator.IsNotNull => " IS NOT NULL",
                 _ => throw new NotImplementedException(),
             };
             _ = result.Append(operand);
         }
         return result.ToString();
+        static string formatOperand(AdvancedSearchOperation operation, object? operand)
+        {
+            if (operation.Field.Type == typeof(int) || operation.Field.Type == typeof(long))
+            {
+                return operand.ToString()!;
+            }
+
+            if (operation.Field.Type == typeof(DateTime) || operation.Field.Type == typeof(DateOnly) || operation.Field.Type == typeof(TimeOnly))
+            {
+                return SqlTypeHelper.FormatDate(operand);
+            }
+            if (ObjectHelper.IsDbNull(operand))
+            {
+                return "null";
+            };
+            return $"'{operand}'";
+        }
     }
 }
 
 public readonly record struct AdvancedSearchField(string Name, TypePath Type);
 
-public readonly record struct AdvancedSearchOperation(AdvancedSearchField Field, AdvancedSearchFieldOperator Operator, IEnumerable<object> Parameters);
+public readonly record struct AdvancedSearchOperation(AdvancedSearchField Field, AdvancedSearchFieldOperator Operator, IEnumerable<object?>? Parameters = null);
 
 public enum AdvancedSearchFieldOperator
 {
@@ -46,10 +67,26 @@ public enum AdvancedSearchFieldOperator
     StartsWith,
     EndsWith,
     Equals,
-    NotEquals
+    NotEquals,
+    IsNull,
+    IsNotNull,
 }
 
-public sealed class AdvancedSearchViewModel
+public sealed class AdvancedSearchViewModel : INew<AdvancedSearchViewModel>
 {
-    public HashSet<AdvancedSearchOperation> Operations { get; } = [];
+    private AdvancedSearchViewModel()
+    {
+    }
+
+    public IEnumerable<AdvancedSearchOperation> Operations => this._operations;
+    private HashSet<AdvancedSearchOperation> _operations { get; } = [];
+
+    public static AdvancedSearchViewModel New() =>
+        new();
+
+    public AdvancedSearchViewModel AddOperation(AdvancedSearchOperation operation)
+    {
+        _ = this._operations.Add(operation);
+        return this;
+    }
 }
--- tests/ConsoleApp/Program.cs
+++ tests/ConsoleApp/Program.cs
@@ -17,14 +17,20 @@ internal class Program
 
         var idBiggerThan2 = new AdvancedSearchOperation(id, AdvancedSearchFieldOperator.IsBiggerThan, [2]);
         var idLessThan10 = new AdvancedSearchOperation(id, AdvancedSearchFieldOperator.IsLessThan, [10]);
+        var nameIsNotNull = new AdvancedSearchOperation(fName, AdvancedSearchFieldOperator.IsNotNull);
         var nameContainsAli = new AdvancedSearchOperation(fName, AdvancedSearchFieldOperator.Contains, ["Ali"]);
         var lastEndsWithZadeh = new AdvancedSearchOperation(lName, AdvancedSearchFieldOperator.EndsWith, ["Zadeh"]);
+        var lastIsNull = new AdvancedSearchOperation(fName, AdvancedSearchFieldOperator.IsNull);
+        var birthDateEqualsDate = new AdvancedSearchOperation(birthDate, AdvancedSearchFieldOperator.EndsWith, [DateOnly.Parse("10/12/1977")]);
 
-        var model = new AdvancedSearchViewModel();
-        _ = model.Operations.Add(idBiggerThan2);
-        _ = model.Operations.Add(idLessThan10);
-        _ = model.Operations.Add(nameContainsAli);
-        _ = model.Operations.Add(lastEndsWithZadeh);
+        var model = AdvancedSearchViewModel.New()
+            .AddOperation(idBiggerThan2)
+            .AddOperation(idLessThan10)
+            .AddOperation(nameContainsAli)
+            .AddOperation(lastEndsWithZadeh)
+            .AddOperation(birthDateEqualsDate)
+            .AddOperation(lastIsNull)
+            .AddOperation(nameIsNotNull);
 
         var code = AdvancedSearchImp.GenerateCode(model);
         Console.WriteLine(code);
