50c55a3c7ecb5ac03843634354e5b59c06c46312
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-03-03 21:55:14 +0330

    Working on UI bugs

--- tests/TestProject/Tests/Services/FunctionalityServiceTest.cs
+++ tests/TestProject/Tests/Services/FunctionalityServiceTest.cs
@@ -1,9 +1,4 @@
-﻿
-
-
-
-
-namespace InfraTestProject.Tests.Services;
+﻿namespace TestProject.Tests.Services;
 
 public sealed class FunctionalityServiceTest(IFunctionalityService service, IFunctionalityCodeService codeService)
 {
@@ -14,7 +9,7 @@ public sealed class FunctionalityServiceTest(IFunctionalityService service, IFun
     {
         // Assign
         using var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(60));
-        var model = await service.GenerateViewModelAsync(CreateModel(), tokenSource.Token);
+        var model = await codeService.GenerateViewModelAsync(CreateModel(), tokenSource.Token);
 
         // Act
         var actual = codeService.GenerateCodes(model!, new(true, tokenSource.Token));
@@ -44,12 +39,12 @@ public sealed class FunctionalityServiceTest(IFunctionalityService service, IFun
         var model = CreateModel();
 
         // Act
-        var actual = await service.GenerateViewModelAsync(model, tokenSource.Token);
+        var actual = await codeService.GenerateViewModelAsync(model, tokenSource.Token);
 
         // Assert
         if (!actual.IsSucceed)
         {
-            Assert.Fail(actual.Message ?? $"{nameof(service.GenerateViewModelAsync)} failed.");
+            Assert.Fail(actual.Message ?? $"{nameof(codeService.GenerateViewModelAsync)} failed.");
         }
     }
 
@@ -59,7 +54,7 @@ public sealed class FunctionalityServiceTest(IFunctionalityService service, IFun
     {
         // Assign
         using var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(3600));
-        var model = await service.GenerateViewModelAsync(CreateModel(), tokenSource.Token);
+        var model = await codeService.GenerateViewModelAsync(CreateModel(), tokenSource.Token);
 
         // Act
         var result = await service.InsertAsync(model!, token: tokenSource.Token);
