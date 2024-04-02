c5636160f7c94c5af6774bae674bc6ee205a7e91
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-04-02 23:59:21 +0330

    Checkpoint: Working on ASP.NET API project

--- src/Bcl/src/CoreLib/Data/Ado/AdoGenericRepository.cs
+++ src/Bcl/src/CoreLib/Data/Ado/AdoGenericRepository.cs
@@ -1,47 +1,10 @@
 ﻿using Library.Data.SqlServer;
 using Library.Interfaces;
 using Library.Results;
-using Library.Types;
-using Library.Validations;
 
 namespace Library.Data.Ado;
 
-public sealed class AdoGenericRepository<TEntity, TId> : AdoRepositoryBase, IAsyncCrud<TEntity, TId>
-    where TEntity : new()
-{
-    public AdoGenericRepository(in Sql sql) : base(sql)
-    {
-    }
-
-    public AdoGenericRepository(in string connectionString) : base(connectionString)
-    {
-    }
-
-    public Task<Result> DeleteAsync(TEntity model, bool persist = true, CancellationToken token = default)
-    {
-        var idColumn = Sql.FindIdColumn<TEntity>();
-        Check.MustBeArgumentNotNull(idColumn);
-        this.Sql.ExecuteNonQuery("")
-    }
-
-    public async Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken token = default)
-    {
-        var result = await this.OnGetAll<TEntity>(token).ToListAsync(token);
-        return result.AsReadOnly();
-    }
-
-    public Task<TEntity?> GetByIdAsync(TId id, CancellationToken token = default)
-    {
-        Check.MustBeArgumentNotNull(id);
-        return this.OnGetByIdAsync<TEntity>(id, token);
-    }
-
-    public Task<Result<TEntity>> InsertAsync(TEntity model, bool persist = true, CancellationToken token = default) => throw new NotImplementedException();
-
-    public Task<Result<TEntity>> UpdateAsync(TId id, TEntity model, bool persist = true, CancellationToken token = default) => throw new NotImplementedException();
-}
-
-public sealed class AdoGenericRepository<TEntity> : AdoRepositoryBase, IAsyncCrud<TEntity>
+public sealed class AdoGenericRepository<TEntity> : AdoRepositoryBase<TEntity>, IAsyncCrud<TEntity>
     where TEntity : new()
 {
     public AdoGenericRepository(in Sql sql) : base(sql)
@@ -54,16 +17,18 @@ public sealed class AdoGenericRepository<TEntity> : AdoRepositoryBase, IAsyncCru
 
     public Task<Result> DeleteAsync(TEntity model, bool persist = true, CancellationToken token = default) => throw new NotImplementedException();
 
+    Task<Result<int>> IAsyncWrite<TEntity, long>.DeleteAsync(TEntity model, bool persist, CancellationToken token) => throw new NotImplementedException();
+
     public async Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken token = default)
     {
-        var result = await this.OnGetAll<TEntity>(token).ToListAsync(token);
+        var result = await this.OnGetAll(token).ToListAsync(token);
         return result.AsReadOnly();
     }
 
     public Task<TEntity?> GetByIdAsync(long id, CancellationToken token = default)
     {
         ArgumentNullException.ThrowIfNull(id);
-        return this.OnGetByIdAsync<TEntity>(id, token);
+        return this.OnGetByIdAsync(id, token);
     }
 
     public Task<Result<TEntity>> InsertAsync(TEntity model, bool persist = true, CancellationToken token = default) => throw new NotImplementedException();
--- src/Bcl/src/CoreLib/Data/SqlServer/Builders/SqlStatementBuilder.Common.cs
+++ src/Bcl/src/CoreLib/Data/SqlServer/Builders/SqlStatementBuilder.Common.cs
@@ -1,9 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Reflection;
-
-using Library.CodeGeneration;
+﻿using Library.CodeGeneration;
 using Library.Data.SqlServer.Builders.Bases;
-using Library.Validations;
 
 namespace Library.Data.SqlServer;
 
@@ -15,34 +11,12 @@ public partial class SqlStatementBuilder
     public static TCommandStatement ReturnId<TCommandStatement>([DisallowNull] this TCommandStatement statement, bool returnId = true)
         where TCommandStatement : ICommandStatement => statement.With(x => x.ReturnId = returnId);
 
+    private static (string? Schema, string Name, IEnumerable<(string Name, TypePath Type)> Columns) GetTable<TTable>()
+        => GetTable(typeof(TTable));
+
     private static (string? Schema, string Name, IEnumerable<(string Name, TypePath Type)> Columns) GetTable(Type type)
     {
-        Check.MustBeArgumentNotNull(type);
-
-        var tableAttribute = type.GetCustomAttribute<TableAttribute>();
-
-        var schema = tableAttribute?.Schema;
-        var tableName = tableAttribute?.Name ?? type.Name;
-        var columns = type.GetProperties().Where(x => x.GetCustomAttribute<NotMappedAttribute>() == null).Select(x =>
-        {
-            string name;
-            TypePath type;
-            int order;
-            var columnAttribute = x.GetCustomAttribute<ColumnAttribute>();
-            if (columnAttribute is { } attrib)
-            {
-                name = attrib.Name ?? x.Name;
-                type = attrib.TypeName ?? x!.DeclaringType!.FullName!;
-                order = attrib.Order;
-            }
-            else
-            {
-                name = x.Name;
-                type = x.PropertyType;
-                order = 0;
-            }
-            return (name, type, order);
-        }).OrderBy(x => x.order).Select(x => (x.name, x.type));
-        return (schema, tableName, columns);
+        var table = Sql.GetTable(type);
+        return (table.Schema(), table.Name(), table.Columns());
     }
 }
