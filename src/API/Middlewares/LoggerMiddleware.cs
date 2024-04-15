e031f1f19b421dda22fa3e4de4bcd44e846e4df5
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-04-15 22:33:08 +0330

    Checkpoint. Testing `GetProductById` API

---
+++ src/API/Middlewares/LoggerMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace API.Middlewares;
+
+public class LoggerMiddleware(RequestDelegate next, ILogger<LoggerMiddleware> logger)
+{
+    private readonly ILogger<LoggerMiddleware> _logger = logger;
+    private readonly RequestDelegate _next = next;
+
+    public Task Invoke(HttpContext httpContext)
+    {
+        var timer = Stopwatch.StartNew();
+        try
+        {
+            this._logger.LogDebug("Executing {api}", httpContext.Request.Path);
+            return this._next(httpContext);
+        }
+        finally
+        {
+            timer.Stop();
+            this._logger.LogTrace("Executed {api} in {elapsed}", httpContext.Request.Path, timer.Elapsed);
+        }
+    }
+}
+
+public static class LoggerMiddlewareExtensions
+{
+    public static IApplicationBuilder UseLoggerMiddleware(this IApplicationBuilder builder)
+        => builder.UseMiddleware<LoggerMiddleware>();
+}
