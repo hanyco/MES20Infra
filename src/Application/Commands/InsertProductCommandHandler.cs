be42d8800e5405d59abd8e3612451f7b469a8c21
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-04-15 23:31:42 +0330

    Checkpoint: working on Insert API

---
+++ src/Application/Commands/InsertProductCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Commands;
+using Domain.Dtos;
+
+using Library.Results;
+
+namespace Application.Commands;
+
+internal partial class InsertProductCommandHandler
+{
+    private partial Task OnHanded(InsertProductCommand request, Result<Product> response, CancellationToken cancellationToken)
+        => Task.CompletedTask;
+
+    private partial Task OnHanding(InsertProductCommand request, CancellationToken cancellationToken)
+        => Task.CompletedTask;
+}
