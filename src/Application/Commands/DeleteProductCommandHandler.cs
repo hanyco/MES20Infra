ea3ccabe74fa5375735a62d766ed9167b9355dd8
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-05-19 00:10:23 +0330

    🎉🎊🎃🎇🎈🎁🎄

---
+++ src/Application/Commands/DeleteProductCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Commands;
+
+using Library.Results;
+
+namespace Application.Commands;
+internal partial class DeleteProductCommandHandler
+{
+    private partial Task OnHanded(DeleteProductCommand request, Result response, CancellationToken cancellationToken)
+        => Task.CompletedTask;
+    private partial Task OnHanding(DeleteProductCommand request, CancellationToken cancellationToken)
+        => Task.CompletedTask;
+}
--- src/Domain/Commands/InsertProductCommand.cs
+++ src/Domain/Commands/InsertProductCommand.cs
@@ -8,7 +8,10 @@ namespace Domain.Commands;
 
 public sealed class InsertProductCommand : IRequest<Result<long?>>
 {
-    public InsertProductCommand(Product product) => this.Product = product;
+    public InsertProductCommand(Product product)
+    {
+        this.Product = product;
+    }
 
     public Product Product { get; }
 }
