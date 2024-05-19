cff8952fca4f94da5c0b73eee6915d269e978eaa
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-05-19 23:57:52 +0330

    Generating Controller code

---
+++ src/CodeGenerator/Contracts/Services/IControllerCodeService.cs
@@ -0,0 +1,19 @@
+﻿using HanyCo.Infra.CodeGen.Contracts.CodeGen.ViewModels;
+using HanyCo.Infra.CodeGeneration.FormGenerator.Bases;
+
+using Library.DesignPatterns.Markers;
+using Library.Interfaces;
+
+namespace HanyCo.Infra.CodeGen.Contracts.Services;
+public interface IControllerCodeService : IBusinessService
+    , ICodeGenerator<ControllerViewModel, ControllerCodeServiceGenerateCodesParameters>
+{
+}
+
+[Immutable]
+public sealed record ControllerCodeServiceGenerateCodesParameters(
+    bool generateGetAll = true,
+    bool generateGetById = true,
+    bool generateInsert = true,
+    bool generateUpdate = true,
+    bool generateDelete = true);
