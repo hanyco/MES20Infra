b766062290c7100b78a98a45880914dc6728bb56
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-09-18 01:14:35 +0330

    Checkpoint: (EoD)

--- src/HanyCo/CodeGenerator/Services/FunctionalityService.CodeGenerator.cs
+++ src/HanyCo/CodeGenerator/Services/FunctionalityService.CodeGenerator.cs
@@ -229,150 +229,16 @@ internal partial class FunctionalityService
 
                 IEnumerable<Result<Codes>> gather(CqrsViewModelBase model)
                 {
-                    var paramsDtoCode = createParams(model);
-                    var resultDtoCode = createResult(model);
-                    var handlerCode = createHandler(model);
+                    var paramsDtoCode = CreateParams(model);
+                    var resultDtoCode = this.CreateResult(model);
+                    var handlerCode = this.CreateHandler(model);
 
                     yield return paramsDtoCode.ToCodes();
                     yield return resultDtoCode.ToCodes();
                     yield return handlerCode.ToCodes();
-
-                    Code createHandler(CqrsViewModelBase model)
-                    {
-                        var category = model switch
-                        {
-                            CqrsQueryViewModel => CodeCategory.Query,
-                            CqrsCommandViewModel => CodeCategory.Command,
-                            _ => throw new NotSupportedException()
-                        };
-                        var handlerClass = new Class(model.GetSegregateHandlerType(null!))
-                        {
-                            AccessModifier = AccessModifier.Internal,
-                            InheritanceModifier = InheritanceModifier.Sealed | InheritanceModifier.Partial
-                        };
-
-                        var cmdPcr = TypePath.New<ICommandProcessor>();
-                        var qryPcr = TypePath.New<IQueryProcessor>();
-                        var dal = TypePath.New<Sql>();
-
-                        var ctor = new Method(model.GetSegregateHandlerType(null!))
-                        {
-                            IsConstructor = true,
-                            Arguments =
-                            {
-                                new(cmdPcr, arg(cmdPcr.Name)),
-                                new(qryPcr, arg(qryPcr.Name)),
-                                new(dal, arg(dal.Name))
-                            },
-                            Body = $"""
-                                    (this.{fld(cmdPcr.Name)}, this.{fld(qryPcr.Name)}) = ({arg(cmdPcr.Name)}, {arg(qryPcr.Name)});
-                                    this.{fld(dal.Name)} = {arg(dal.Name)};
-                                    """
-                        };
-                        
-                        var paramsType = model.GetSegregateParamsType(null!);
-                        var resultType = model.GetSegregateResultParamsType(null!);
-                        var baseType = category switch
-                        {
-                            CodeCategory.Query => TypePath.New(typeof(IQueryHandler<,>).FullName!, [paramsType.FullName, resultType.FullName]),
-                            CodeCategory.Command => TypePath.New(typeof(ICommandHandler<,>).FullName!, [paramsType.FullName, resultType.FullName]),
-                            _ => throw new NotSupportedException()
-                        };
-                        _ = handlerClass.AddBaseType(baseType)
-                            .AddMember(new Field(fld(cmdPcr.Name), cmdPcr) { AccessModifier = IField.DefaultAccessModifier })
-                            .AddMember(new Field(fld(qryPcr.Name), qryPcr) { AccessModifier = IField.DefaultAccessModifier })
-                            .AddMember(new Field(fld(dal.Name), dal) { AccessModifier = IField.DefaultAccessModifier });
-                        _ = handlerClass.AddMember(ctor);
-
-                        // Create namespace
-                        var ns = INamespace.New(model.CqrsNameSpace)
-                            .AddUsingNameSpace(cmdPcr.GetNameSpaces())
-                            .AddUsingNameSpace(qryPcr.GetNameSpaces())
-                            .AddUsingNameSpace(dal.GetNameSpaces())
-                            .AddUsingNameSpace(paramsType.GetNameSpaces())
-                            .AddUsingNameSpace(resultType.GetNameSpaces())
-                            .AddUsingNameSpace(baseType.GetNameSpaces())
-                            .AddType(handlerClass);
-
-                        var codeStatement = this._generatorEngine.Generate(ns);
-                        var code = Code.New(handlerClass.Name, Languages.CSharp, codeStatement, true).SetCategory(category);
-                        return code;
-                    }
-                    Code createParams(CqrsViewModelBase mode)
-                    {
-                        var segregateType = mode.GetSegregateParamsType(null);
-                        var props = mode.ParamsDto.Properties
-                            .Select(x => (Name: x.Name!, Type: x.IsList is true ? TypePath.New(typeof(List<>).FullName!, [x.TypeFullName]) : TypePath.New(x.TypeFullName))).ToList();
-                        var segregateClass = new Class(segregateType.Name)
-                        {
-                            InheritanceModifier = InheritanceModifier.Sealed | InheritanceModifier.Partial
-                        };
-                        props.ForEach(x => segregateClass.AddProperty(x.Name, x.Type));
-
-                        var ctor = new Method(segregateType.Name) { IsConstructor = true };
-                        props.ForEach(x => ctor.AddArgument(x.Type, arg(x.Name)));
-                        var body = new StringBuilder();
-                        props.ForEach(x => body.AppendLine($"this.{x.Name} = {arg(x.Name)};"));
-                        ctor.Body = body.ToString();
-                        segregateClass.AddMember(ctor);
-
-                        var defCtor = new Method(segregateType.Name) { IsConstructor = true };
-                        segregateClass.AddMember(defCtor);
-
-                        var usings = segregateType.GetNameSpaces()
-                            .AddRangeImmuted(props.Select(x => x.Type.GetNameSpaces()).SelectAll())
-                            .Except([segregateType.NameSpace]);
-
-                        var ns = INamespace.New(segregateType.NameSpace)
-                            .AddUsingNameSpace(usings)
-                        .AddType(segregateClass);
-
-                        var codeStatement = this._generatorEngine.Generate(ns);
-                        var code = Code.New(segregateType.Name, Languages.CSharp, codeStatement, true).SetCategory(CodeCategory.Dto);
-                        return code;
-                    }
-                    Code createResult(CqrsViewModelBase mode)
-                    {
-                        var segregateType = mode.GetSegregateResultParamsType(null);
-                        var props = mode.ResultDto.Properties
-                            .Select(x => (Name: x.Name!, Type: x.IsList is true ? TypePath.New(typeof(List<>).FullName!, [x.TypeFullName]) : TypePath.New(x.TypeFullName))).ToList();
-                        var segregateClass = new Class(segregateType.Name)
-                        {
-                            InheritanceModifier = InheritanceModifier.Sealed | InheritanceModifier.Partial
-                        };
-                        props.ForEach(x => segregateClass.AddProperty(x.Name, x.Type));
-
-                        var ctor = new Method(segregateType.Name) { IsConstructor = true };
-                        props.ForEach(x => ctor.AddArgument(x.Type, arg(x.Name)));
-                        var body = new StringBuilder();
-                        props.ForEach(x => body.AppendLine($"this.{x.Name} = {arg(x.Name)};"));
-                        ctor.Body = body.ToString();
-                        segregateClass.AddMember(ctor);
-
-                        var defCtor = new Method(segregateType.Name) { IsConstructor = true };
-                        segregateClass.AddMember(defCtor);
-
-                        var usings = segregateType.GetNameSpaces()
-                            .AddRangeImmuted(props.Select(x => x.Type.GetNameSpaces()).SelectAll())
-                            .Except([segregateType.NameSpace]);
-
-                        var ns = INamespace.New(segregateType.NameSpace)
-                            .AddUsingNameSpace(usings)
-                        .AddType(segregateClass);
-
-                        var codeStatement = this._generatorEngine.Generate(ns);
-                        var code = Code.New(segregateType.Name, Languages.CSharp, codeStatement, true).SetCategory(CodeCategory.Dto);
-                        return code;
-                    }
                 }
             }
         }
-        static string arg(in string name)
-            => TypeMemberNameHelper.ToArgName(name);
-        static string fld(in string name)
-            => TypeMemberNameHelper.ToFieldName(name);
-        static string prp(in string name)
-            => TypeMemberNameHelper.ToPropName(name);
         static Result<Codes?> aggregatedResults(IReadOnlyList<Result<Codes>> results)
         {
             Result<Codes?> result;
@@ -391,5 +257,146 @@ internal partial class FunctionalityService
 
             return result;
         }
+    }
+
+    private static string arg(in string name) => TypeMemberNameHelper.ToArgName(name);
+
+    private static string fld(in string name) => TypeMemberNameHelper.ToFieldName(name);
+
+    private static string prp(in string name) => TypeMemberNameHelper.ToPropName(name);
+
+    private Code CreateHandler(CqrsViewModelBase model)
+    {
+        var category = model switch
+        {
+            CqrsQueryViewModel => CodeCategory.Query,
+            CqrsCommandViewModel => CodeCategory.Command,
+            _ => throw new NotSupportedException()
+        };
+        var handlerType = model.GetSegregateHandlerType(null!);
+        var handlerClass = new Class(handlerType)
+        {
+            AccessModifier = AccessModifier.Internal,
+            InheritanceModifier = InheritanceModifier.Sealed | InheritanceModifier.Partial
+        };
+
+        var cmdPcr = TypePath.New<ICommandProcessor>();
+        var qryPcr = TypePath.New<IQueryProcessor>();
+        var dal = TypePath.New<Sql>();
+
+        var ctor = new Method(handlerType)
+        {
+            IsConstructor = true,
+            Arguments =
+            {
+                new(cmdPcr, arg(cmdPcr.Name)),
+                new(qryPcr, arg(qryPcr.Name)),
+                new(dal, arg(dal.Name))
+            },
+            Body = $"""
+                (this.{fld(cmdPcr.Name)}, this.{fld(qryPcr.Name)}) = ({arg(cmdPcr.Name)}, {arg(qryPcr.Name)});
+                this.{fld(dal.Name)} = {arg(dal.Name)};
+                """
+        };
+
+        var paramsType = model.GetSegregateParamsType(null!);
+        var resultType = model.GetSegregateResultParamsType(null!);
+        var baseType = category switch
+        {
+            CodeCategory.Query => TypePath.New(typeof(IQueryHandler<,>).FullName!, [paramsType.FullName, resultType.FullName]),
+            CodeCategory.Command => TypePath.New(typeof(ICommandHandler<,>).FullName!, [paramsType.FullName, resultType.FullName]),
+            _ => throw new NotSupportedException()
+        };
+        _ = handlerClass.AddBaseType(baseType)
+            .AddMember(new Field(fld(cmdPcr.Name), cmdPcr) { AccessModifier = IField.DefaultAccessModifier })
+            .AddMember(new Field(fld(qryPcr.Name), qryPcr) { AccessModifier = IField.DefaultAccessModifier })
+            .AddMember(new Field(fld(dal.Name), dal) { AccessModifier = IField.DefaultAccessModifier });
+        _ = handlerClass.AddMember(ctor);
+
+        var usings = handlerType.GetNameSpaces()
+            .AddRangeImmuted(cmdPcr.GetNameSpaces())
+            .AddRangeImmuted(qryPcr.GetNameSpaces())
+            .AddRangeImmuted(dal.GetNameSpaces())
+            .AddRangeImmuted(paramsType.GetNameSpaces())
+            .AddRangeImmuted(resultType.GetNameSpaces())
+            .AddRangeImmuted(baseType.GetNameSpaces())
+            .Except([model.CqrsNameSpace]);
+        var ns = INamespace.New(model.CqrsNameSpace)
+            .AddType(handlerClass)
+            .AddUsingNameSpace(usings);
+        
+        var codeStatement = this._generatorEngine.Generate(ns);
+        var code = Code.New(handlerClass.Name, Languages.CSharp, codeStatement, true).SetCategory(category);
+        return code;
+    }
+
+    private Code CreateParams(CqrsViewModelBase model)
+    {
+        var segregateType = model.GetSegregateParamsType(null);
+
+        var segregateClass = new Class(segregateType.Name)
+        {
+            InheritanceModifier = InheritanceModifier.Sealed | InheritanceModifier.Partial
+        }.AddBaseType(TypePath.New(typeof(IQuery<>).FullName!, [model.ResultDto.FullName]));
+
+        var props = model.ParamsDto.Properties.Select(x => (Name: x.Name!, Type: x.IsList is true ? TypePath.New(typeof(List<>).FullName!, [x.TypeFullName]) : TypePath.New(x.TypeFullName))).ToList();
+        if (props.Any())
+        {
+            props.ForEach(x => segregateClass.AddProperty(x.Name, x.Type));
+
+            var ctor = new Method(segregateType.Name) { IsConstructor = true };
+            props.ForEach(x => ctor.AddArgument(x.Type, arg(x.Name)));
+            var body = new StringBuilder();
+            props.ForEach(x => body.AppendLine($"this.{x.Name} = {arg(x.Name)};"));
+            ctor.Body = body.ToString();
+            _ = segregateClass.AddMember(ctor);
+        }
+
+        var usings = segregateType.GetNameSpaces()
+            .AddRangeImmuted(segregateClass.BaseTypes.Select(x => x.GetNameSpaces()).SelectAll())
+            .AddRangeImmuted(props.Select(x => x.Type.GetNameSpaces()).SelectAll())
+            .Except([segregateType.NameSpace]);
+
+        var ns = INamespace.New(segregateType.NameSpace)
+            .AddUsingNameSpace(usings)
+            .AddType(segregateClass);
+
+        var codeStatement = this._generatorEngine.Generate(ns);
+        var code = Code.New(segregateType.Name, Languages.CSharp, codeStatement, true).SetCategory(CodeCategory.Dto);
+        return code;
+    }
+
+    private Code CreateResult(CqrsViewModelBase mode)
+    {
+        var segregateType = mode.GetSegregateResultParamsType(null);
+        var segregateClass = new Class(segregateType.Name)
+        {
+            InheritanceModifier = InheritanceModifier.Sealed | InheritanceModifier.Partial
+        };
+
+        var props = mode.ResultDto.Properties.Select(x => (Name: x.Name!, Type: x.IsList is true ? TypePath.New(typeof(List<>).FullName!, [x.TypeFullName]) : TypePath.New(x.TypeFullName))).ToList();
+        if (props.Any())
+        {
+            props.ForEach(x => segregateClass.AddProperty(x.Name, x.Type));
+
+            var ctor = new Method(segregateType.Name) { IsConstructor = true };
+            props.ForEach(x => ctor.AddArgument(x.Type, arg(x.Name)));
+            var body = new StringBuilder();
+            props.ForEach(x => body.AppendLine($"this.{x.Name} = {arg(x.Name)};"));
+            ctor.Body = body.ToString();
+            _ = segregateClass.AddMember(ctor);
+        }
+
+        var usings = segregateType.GetNameSpaces()
+            .AddRangeImmuted(props.Select(x => x.Type.GetNameSpaces()).SelectAll())
+            .Except([segregateType.NameSpace]);
+
+        var ns = INamespace.New(segregateType.NameSpace)
+            .AddUsingNameSpace(usings)
+            .AddType(segregateClass);
+
+        var codeStatement = this._generatorEngine.Generate(ns);
+        var code = Code.New(segregateType.Name, Languages.CSharp, codeStatement, true).SetCategory(CodeCategory.Dto);
+        return code;
     }
 }
