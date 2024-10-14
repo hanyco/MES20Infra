0b61dd0437540668cc58247c3260801f80a74863
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-10-14 23:45:17 +0330

    Checkpoint: Implementing Microsoft Identity...

---
+++ src/Application/Identity/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Identity;
+
+using Microsoft.AspNetCore.Identity;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Identity;
+public interface IUserService
+{
+    Task<IdentityResult> CreateAsync(ApplicationUser user, string password);
+    Task<ApplicationUser?> FindByNameAsync(string username);
+}
+
+public class UserService(UserManager<ApplicationUser> userManager) : IUserService
+{
+    private readonly UserManager<ApplicationUser> _userManager = userManager;
+
+    public async Task<IdentityResult> CreateAsync(ApplicationUser user, string password)
+    {
+        return await _userManager.CreateAsync(user, password);
+    }
+
+    public async Task<ApplicationUser?> FindByNameAsync(string username)
+    {
+        return await _userManager.FindByNameAsync(username);
+    }
+}
---
+++ src/Domain/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Identity;
+public class ApplicationUser : IdentityUser;
