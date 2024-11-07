515900bf3ab9dfea7ba1a9fe97d9265ff610c969
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-11-08 03:11:37 +0330

    Woking on access pemissions

--- src/Application/Features/Permissions/Repositories/AccessPermissionRepository.cs
+++ src/Application/Features/Permissions/Repositories/AccessPermissionRepository.cs
@@ -15,7 +15,7 @@ internal sealed class AccessPermissionRepository(IdentityDbContext context) : IA
     /// <param name="userId">User's ID</param>
     /// <param name="entityId">Entity's ID</param>
     /// <returns>Access permission if found; otherwise, null.</returns>
-    public async Task<AccessPermission?> GetAccessPermissionAsync(string userId, long entityId)
+    public async Task<AccessPermission?> GetAccessPermission(string userId, long entityId)
     {
         var query = from ap in _context.AccessPermissions
                     where ap.UserId == userId && ap.EntityId == entityId
@@ -32,7 +32,7 @@ internal sealed class AccessPermissionRepository(IdentityDbContext context) : IA
     /// <param name="entityId">Entity's ID</param>
     /// <returns>Parent access permission if found; otherwise, null.</returns>
     // Revised GetParentPermissionAsync method
-    public async Task<AccessPermission?> GetParentPermissionAsync(string userId, long entityId)
+    public async Task<AccessPermission?> GetParentPermission(string userId, long entityId)
     {
         // Query to find the parent AccessPermission based on UserId and ParentId of the entity
         var query = from ap in _context.AccessPermissions
@@ -51,7 +51,7 @@ internal sealed class AccessPermissionRepository(IdentityDbContext context) : IA
         return null; // Return null if there's no parent
     }
 
-    public async Task<AccessPermission?> GetAccessPermissionWithParentAsync(string userId, long entityId)
+    public async Task<AccessPermission?> GetAccessPermissionWithParent(string userId, long entityId)
     {
         var query = from p in _context.AccessPermissions
                     where p.UserId == userId && (p.EntityId == entityId || p.EntityId == _context.AccessPermissions.FirstOrDefault(x => x.EntityId == entityId).ParentId)
