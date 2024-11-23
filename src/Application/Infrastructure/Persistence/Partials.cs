568ade1b7a27312b582a93f216df299dd06a07be
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-11-23 23:04:15 +0330

    Checkpoint: Working on Claims implementation for about 15 days.

--- src/Application/Infrastructure/Persistence/Partials.cs
+++ src/Application/Infrastructure/Persistence/Partials.cs
@@ -3,52 +3,56 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Application.Infrastructure.Persistence;
-partial class IdentityDbContext
+
+public partial class IdentityDbContext
 {
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<ApplicationUser>(entity =>
+        _ = modelBuilder.Entity<ApplicationUser>(entity =>
         {
-            entity.ToTable("ApplicationUsers", "Identity");
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.UserName).IsRequired().HasMaxLength(256);
-            entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
-            entity.Property(e => e.Email).HasMaxLength(256);
-            entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+            _ = entity.ToTable("ApplicationUsers", "Identity")
+                .HasKey(e => e.Id);
+            _ = entity.Property(e => e.UserName)
+                .IsRequired()
+                .HasMaxLength(256);
+            _ = entity.Property(e => e.NormalizedUserName)
+                .HasMaxLength(256);
+            _ = entity.Property(e => e.Email)
+                .HasMaxLength(256);
+            _ = entity.Property(e => e.NormalizedEmail)
+                .HasMaxLength(256);
 
-            entity.HasMany<IdentityUserLogin<string>>().WithOne()
+            _ = entity.HasMany<IdentityUserLogin<string>>().WithOne()
                 .HasForeignKey(e => e.UserId).IsRequired();
-            entity.HasMany<IdentityUserToken<string>>().WithOne()
+            _ = entity.HasMany<IdentityUserToken<string>>().WithOne()
                 .HasForeignKey(e => e.UserId).IsRequired();
-            entity.HasMany<IdentityUserClaim<string>>().WithOne()
+            _ = entity.HasMany<IdentityUserClaim<string>>().WithOne()
                 .HasForeignKey(e => e.UserId).IsRequired();
         });
 
-        modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
+        _ = modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
         {
-            entity.HasKey(login => new { login.LoginProvider, login.ProviderKey });
-            entity.ToTable("AspNetUserLogins", "Identity");
+            _ = entity.HasKey(login => new { login.LoginProvider, login.ProviderKey });
+            _ = entity.ToTable("AspNetUserLogins", "Identity");
+            _ = entity.HasOne<ApplicationUser>()
+                .WithMany()
+                .HasForeignKey(login => login.UserId)
+                .IsRequired();
         });
 
-        modelBuilder.Entity<IdentityUserToken<string>>(entity =>
+        _ = modelBuilder.Entity<IdentityUserToken<string>>(entity =>
         {
-            entity.HasKey(token => new { token.UserId, token.LoginProvider, token.Name });
-            entity.ToTable("AspNetUserTokens", "Identity");
+            _ = entity.HasKey(token => new { token.UserId, token.LoginProvider, token.Name });
+            _ = entity.ToTable("AspNetUserTokens", "Identity");
         });
 
-        modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+        _ = modelBuilder.Entity<IdentityUserRole<string>>(entity =>
         {
-            entity.HasNoKey();
-            entity.ToTable((string?)null);
+            _ = entity.HasNoKey();
+            _ = entity.ToTable((string?)null);
         });
     }
 
